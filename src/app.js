424548c33d73d24e12d8a6e7c5fe4ea1df9e5dfc
Emanuel Alves <emanuelazvdo@gmail.com>
2020-06-21 20:51:06 -0300

    adicionando CRUD e regras de repositórios e likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,66 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const newRepository = {
+    id: uuid(),
+    likes: 0,
+    title,
+    url,
+    techs
+  };
+
+  repositories.push(newRepository);
+  return response.json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repositoryIndex = findRepositoryIndex(id);
+
+  repositories[repositoryIndex] = {
+    ...repositories[repositoryIndex],
+    title,
+    url,
+    techs
+  };
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = findRepositoryIndex(id);
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).json({ message: 'Repository removed' });
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = findRepositoryIndex(id);
+
+  repositories[repositoryIndex].likes++;
+
+  return response.json(repositories[repositoryIndex]);
 });
+
+const findRepositoryIndex = (index) => {
+  const repositoryIndex = repositories.findIndex(el => el.id === index);
+
+  if (repositoryIndex < 0) {
+    const error = new Error();
+    error.status = 400;
+    throw error;
+  }
+
+  return repositoryIndex;
+}
 
 module.exports = app;
